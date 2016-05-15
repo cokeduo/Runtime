ca2430c54405f0b9084f27eafd213add3104e91c
王铎 <cokeduo@163.com>
2016-05-15 16:38:49 +0800

    Initial Commit

---
+++ RuntimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeTest
+//
+//  Created by change009 on 16/5/15.
+//  Copyright © 2016年 change009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeTest
+//
+//  Created by change009 on 16/5/15.
+//  Copyright © 2016年 change009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
